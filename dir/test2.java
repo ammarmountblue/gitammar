298144782ecda525b37e6cd235694085fefd1d03
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-08 11:36:37 +0530

    starting with branches

--- dir/test2.java
+++ dir/test2.java
@@ -5,10 +5,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("Hello from test2 class!");
         System.out.println("This is test2 class.");
-        System.out.println("Adding another line in test2 class.");
-        System.out.println("Goodbye from test2 class!");
-        System.out.println("End of test2 class output.");
-        System.out.println("Test2 class execution complete.");
-        System.out.println("Exiting test2 class.");
+       
     }
 }
--- text.java
+++ text.java
@@ -3,9 +3,6 @@ public class text {
         System.out.println("Hello from text class!");
         System.out.println("This is text class.");
         System.out.println("Adding another line in text class.");
-        System.out.println("Goodbye from text class!");
-        System.out.println("End of text class output.");
-        System.out.println("Text class execution complete.");
-        System.out.println("Exiting text class.");
+        
     }
 }
