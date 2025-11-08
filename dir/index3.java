462801738c4a9037a059cdf3e5f07c7ce20bf456
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-08 09:41:32 +0530

    did some changes in index3.java

--- dir/index3.java
+++ dir/index3.java
@@ -3,5 +3,9 @@ package dir;
 public class index3 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Hello from index3!");
     }
+
+    
 }
