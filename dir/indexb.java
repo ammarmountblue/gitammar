0a6b2c22d7492c458e50b22a0da20a62c0a80b95
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-08 11:23:20 +0530

    done

---
+++ dir/indexb.java
@@ -0,0 +1,12 @@
+package dir;
+
+public class indexb {
+    public static void main(String[] args) {
+        System.out.println("This is indexb class.");
+        System.out.println("Adding another line in indexb.");
+        System.out.println("Goodbye from indexb!");
+        System.out.println("End of indexb output.");
+        System.out.println("Indexb class execution complete.");
+        System.out.println("Exiting indexb class.");
+    }
+}
--- indexa.java
+++ indexa.java
@@ -1,5 +1,9 @@
 public class indexa {
     public static void main(String[] args) {
+
         System.out.println("Hello from indexa!");
+        System.out.println("Adding another line in indexa.");
+        System.out.println("Goodbye from indexa!");
+        System.out.println("End of indexa output.");
     }   
 }
