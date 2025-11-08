bb19501a4f59d736a0dce876fc4b27c93dff0905
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-08 11:24:12 +0530

    added test.java

---
+++ text.java
@@ -0,0 +1,11 @@
+public class text {
+    public static void main(String[] args) {
+        System.out.println("Hello from text class!");
+        System.out.println("This is text class.");
+        System.out.println("Adding another line in text class.");
+        System.out.println("Goodbye from text class!");
+        System.out.println("End of text class output.");
+        System.out.println("Text class execution complete.");
+        System.out.println("Exiting text class.");
+    }
+}
